c1b90de4f91284bdb12670eadeb8b751cd276e10
Musharraf-712 <musharrafsiddiqui712@gmail.com>
2025-11-09 13:29:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  const table = document.querySelector('#sampleTable')
+	const tRow = document.createElement('tr');
+		 tRow.innerHTML = `<td>New Cell1</td>
+			 <td>NewCell2</td>`;
+	table.prepend(tRow)
+
+ 
 }
